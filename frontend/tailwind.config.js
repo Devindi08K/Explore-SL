4e9e40a83f45925cda9a676a4b39d76304e65e76
Devindi08K <Devindi08@outlook.com>
2025-05-15 12:27:08 +0530

    add new colors for the new design

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
           cream: "#f4ebd0",
           charcoal: "#122620",
           gold: "#d6ad60",
+          TealGreen: "#175873",
+          Teal:"#2b7c85",
+          SeafoamGreen:"#87aca3",
+          RoyalBlue:"#0c1446",
         },
         fontFamily: {
           sans: ['"Poppins"', 'sans-serif'],
